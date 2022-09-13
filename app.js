dc6b3f6b0cda4acb1894ca5d4b74935aad3fa5c0
MaxMoralesIsm <100160568+MaxMoralesIsm@users.noreply.github.com>
2022-09-13 19:16:18 -0300

    Correction exercise 1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const countries = require('./countries.json');
  * @returns {Object}
  */
 function getCountryByIso3(isoCode){
-    let country = find(country => country.iso3 === isoCode);
+    let country = countries.find(country => country.iso3 === isoCode);
     return country
 }
 
